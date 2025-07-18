400318441b1301257c2adb23230ba79eadf96005
Thomaz Lucca <thomazlucca@gmail.com>
2025-07-18 15:06:54 -0300

    adicionando url do render no swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,7 +10,8 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000"
+        url: "http://localhost:3000",
+        url: "https://fiap-fsdt-techchallenge-ii-posts.onrender.com"
       }
     ]
   },
