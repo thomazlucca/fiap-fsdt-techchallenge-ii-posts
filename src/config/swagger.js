3099941a1d659316547ae47122b3f6bcc314ca3e
Gustavo Marques <gustavomsf21@gmail.com>
2025-07-23 20:13:57 -0300

    Ambiente de produção

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,21 +1,18 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API de Blog para Professores",
-      version: "1.0.0",
-      description: "Documentação da API de posts para professores"
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de Blog para Professores",
+            version: "1.0.0",
+            description: "Documentação da API de posts para professores"
+        },
+        servers: [{
+            url: "https://fiap-fsdt-techchallenge-ii-posts.onrender.com"
+        }]
     },
-    servers: [
-      {
-        url: "http://localhost:3000",
-        url: "https://fiap-fsdt-techchallenge-ii-posts.onrender.com"
-      }
-    ]
-  },
-  apis: ["./src/routes/*.js"], // onde estão as anotações
+    apis: ["./src/routes/*.js"], // onde estão as anotações
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
