fa6960a6dec11a670488513d5f09c205e344ace0
Thomaz Lucca <136012682+thomazlucca@users.noreply.github.com>
2025-09-30 10:23:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from "express";
+import cors from "cors";
 import postRoutes from "./routes/postRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://postech-fase-3.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
